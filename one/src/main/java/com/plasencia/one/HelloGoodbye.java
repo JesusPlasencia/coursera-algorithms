9d51c8dd48b553a494c60a64754a482df4bed504
ls-jesusplasencia <jesus.plasenciatoledo@contractors.lesschwab.com>
2024-04-25 17:10:07 -0500

    feat: pushing first version

---
+++ one/src/main/java/com/plasencia/one/HelloGoodbye.java
@@ -0,0 +1,15 @@
+package com.plasencia.one;
+
+public class HelloGoodbye {
+    
+    public static void main(String[] args) {
+        
+        String firstArg = args[0];
+        String secndArg = args[1];
+
+        System.out.println("Hello " + firstArg + " and " + secndArg + ".");
+        System.out.println("Goodbye " + secndArg + " and " + firstArg + ".");
+
+    }
+
+}
---
+++ one/src/main/java/com/plasencia/one/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.plasencia.one;
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World");
+
+    }
+
+}
